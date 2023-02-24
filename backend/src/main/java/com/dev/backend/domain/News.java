41944ed7cacb8694ca36194743448f1e4c6411f7
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-02-24 11:59:10 -0300

    created news entity

---
+++ backend/src/main/java/com/dev/backend/domain/News.java
@@ -0,0 +1,29 @@
+package com.dev.backend.domain;
+
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class News {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String content;
+    private Date dateCreated;
+    private Date dateUpdate;
+    @ManyToOne
+    private User author;
+
+}
