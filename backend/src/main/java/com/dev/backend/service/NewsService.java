cd2c7d256e2aa094a103ce5d95b8f31f0509dae6
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-03 22:03:09 -0300

    created NewsMapping class

--- backend/src/main/java/com/dev/backend/service/NewsService.java
+++ backend/src/main/java/com/dev/backend/service/NewsService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.dev.backend.domain.News;
+import com.dev.backend.mapper.NewsMapper;
 import com.dev.backend.repository.NewsRepository;
 import com.dev.backend.requests.news.NewsPostRequestBody;
 import com.dev.backend.requests.news.NewsPutRequestBody;
@@ -31,15 +32,7 @@ public class NewsService {
     
     public News save(NewsPostRequestBody newsPostRequestBody) {
         newsPostRequestBody.setDateCreated(new Date());
-        News news = News.builder()
-                    .title(newsPostRequestBody.getTitle())
-                    .content(newsPostRequestBody.getContent())
-                    .author(newsPostRequestBody.getAuthor())
-                    .dateCreated(newsPostRequestBody.getDateCreated())
-                    .dateUpadte(newsPostRequestBody.getDateUpadte())
-                    .build();
-
-        return repository.save(news);
+        return repository.save(NewsMapper.INSTANCE.toNews(newsPostRequestBody));
     }
 
     public void delete(long id) {
@@ -47,17 +40,10 @@ public class NewsService {
     }
 
     public void replace(NewsPutRequestBody newsPutRequestBody) {
-        News savedNews = findByIdOrThrowBadRequestException(newsPutRequestBody.getId());
         newsPutRequestBody.setDateUpadte(new Date());
-        News news = News.builder()
-                    .id(savedNews.getId())
-                    .title(newsPutRequestBody.getTitle())
-                    .content(newsPutRequestBody.getContent())
-                    .author(newsPutRequestBody.getAuthor())
-                    .dateCreated(savedNews.getDateCreated())
-                    .dateUpadte(newsPutRequestBody.getDateUpadte())
-                    .build();
-
+        News savedNews = findByIdOrThrowBadRequestException(newsPutRequestBody.getId());
+        News news = NewsMapper.INSTANCE.toNews(newsPutRequestBody);
+        news.setId(savedNews.getId());
         repository.save(news);
     }
 
