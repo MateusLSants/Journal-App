8d2cb7f7ce9168639175599be98355a822607cd1
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-06 13:53:34 -0300

    removed unused imports

--- backend/src/main/java/com/dev/backend/exception/BadRequestExceptionDetails.java
+++ backend/src/main/java/com/dev/backend/exception/BadRequestExceptionDetails.java
@@ -1,9 +1,6 @@
 package com.dev.backend.exception;
 
-import java.time.LocalDateTime;
 
-import lombok.Builder;
-import lombok.Data;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
