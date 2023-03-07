2e62075e0d2e6270ab72a9c5843ee93c2ae49bd0
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-07 08:47:09 -0300

    implemented routing & created users component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'users' },
+  {
+    path: 'users',
+    loadChildren: () => import('./users/users.module').then(m => m.UsersModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
