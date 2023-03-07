6720b5ba54c1a873c6cfda44a11a05246cd59109
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-07 10:55:31 -0300

    implemented user table

--- frontend/src/app/users/users.module.ts
+++ frontend/src/app/users/users.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users/users.component';
@@ -11,7 +12,8 @@ import { UsersComponent } from './users/users.component';
   ],
   imports: [
     CommonModule,
-    UsersRoutingModule
+    UsersRoutingModule,
+    MatTableModule
   ]
 })
 export class UsersModule { }
