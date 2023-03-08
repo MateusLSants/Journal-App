6ba20f7af1366aa1adbb160824f0bcfbcb5a9251
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-08 12:01:02 -0300

    Implementig custom navbar in app.js

---
+++ frontend/src/components/CustomHeader/CustomHeader.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./CustomHeader.css"
+
+function CustomHeader() {
+    return(
+        <body>
+            <nav>
+                <label className="logo">Journal App</label>
+                <ul>
+                    <input type={Text} placeholder="Search"></input>
+                </ul> 
+            </nav>
+        </body>
+    )
+}
+
+export default CustomHeader
