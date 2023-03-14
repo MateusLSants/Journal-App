58ac642ff9676b168eefc545df87badd3a13615d
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-14 09:19:31 -0300

    implements email validation

--- backend/src/main/java/com/dev/backend/domain/requests/user/UserPostRequestBody.java
+++ backend/src/main/java/com/dev/backend/domain/requests/user/UserPostRequestBody.java
@@ -1,5 +1,8 @@
 package com.dev.backend.domain.requests.user;
 
+import org.hibernate.validator.constraints.UniqueElements;
+
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -9,8 +12,10 @@ public class UserPostRequestBody {
     @NotNull(message = "name cannot be null")
     @NotEmpty(message = "name cannot be empty")
     private String name;
+    @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     @NotNull(message = "email cannot be null")
     @NotEmpty(message = "email cannot be empty")
+    @UniqueElements
     private String email;
     @NotNull(message = "password cannot be null")
     @NotEmpty(message = "password cannot be empty")
