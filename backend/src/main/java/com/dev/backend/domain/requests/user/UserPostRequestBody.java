447ae3bbbf8ba6bb8899acea9e87670829a7a815
Mateus Leandro <100031728+MateusLSants@users.noreply.github.com>
2023-03-14 09:30:55 -0300

    implements validation in email fields

--- backend/src/main/java/com/dev/backend/domain/requests/user/UserPostRequestBody.java
+++ backend/src/main/java/com/dev/backend/domain/requests/user/UserPostRequestBody.java
@@ -1,6 +1,5 @@
 package com.dev.backend.domain.requests.user;
 
-import org.hibernate.validator.constraints.UniqueElements;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
@@ -15,7 +14,6 @@ public class UserPostRequestBody {
     @Email(message = "Email is not valid", regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     @NotNull(message = "email cannot be null")
     @NotEmpty(message = "email cannot be empty")
-    @UniqueElements
     private String email;
     @NotNull(message = "password cannot be null")
     @NotEmpty(message = "password cannot be empty")
